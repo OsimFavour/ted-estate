95ba6195c62bf5ed842110a2fca4d8d98c0f4e13
OsimFavour <famos204@gmail.com>
2024-03-22 18:58:44 +0100

    Create Service JSON

---
+++ src/utils/Data.js
@@ -0,0 +1,31 @@
+export const services = [
+    {
+        title: 'Anna Smith',
+        tagline: 'Tempor stet labore',
+        image: 'images/service.png'
+    },
+
+    {
+        title: 'Client Name',
+        tagline: 'Tempor stet labore',
+        image: 'images/service-02.png'
+    },
+
+    {
+        title: 'Client Name',
+        tagline: 'Tempor stet labore',
+        image: 'images/service-03.png'
+    },
+
+    {
+        title: 'Client Name',
+        tagline: 'Tempor stet labore',
+        image: 'images/service-04.png'
+    },
+
+    {
+        title: 'Client Name',
+        tagline: 'Tempor stet labore',
+        image: 'images/service-05.png'
+    }
+]
